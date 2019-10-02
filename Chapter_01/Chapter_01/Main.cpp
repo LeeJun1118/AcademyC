b67c587f1c69bce221e94c1f6664ef8db2970188
ljun1118 <ljun1118@ks.ac.kr>
2019-10-02 17:47:01 +0900

    chapter1

--- Chapter_01/Chapter_01/Main.cpp
+++ Chapter_01/Chapter_01/Main.cpp
@@ -3,14 +3,14 @@
 
 using namespace std;
 
-int MAX = 0;
-int iCount = 0;
+int MAX = 10;
+
 int* piArray;
 
 int main(void)
 {
 	// Enter 칠 때마다 1씩 증가해서 출력되게 
-	piArray = NULL;
+	
 	DWORD dwTime = GetTickCount();
 
 	while (true)
@@ -19,12 +19,30 @@ int main(void)
 		{
 			dwTime = GetTickCount();
 			system("cls");
+			piArray = new int[10];
+
+			for (int i = 0; i < 10; i++)
+			{
+				piArray[i] = i;
+				printf_s("%d\n", piArray[i]);
+			}
 
 			if (GetAsyncKeyState(VK_RETURN))
 			{
-
+				int* iTemp = new int[MAX + 1];
+				for (int i = 0; i < MAX; i++)
+				{
+					iTemp[i] = piArray[i];
+				}
+				iTemp[MAX] = iTemp[MAX - 1] + 1;
+				printf_s("%d\n", iTemp[MAX]);
+				MAX++;
 			}
 		}
+		
+			
+			
+		
 	}
 
 	return 0;
